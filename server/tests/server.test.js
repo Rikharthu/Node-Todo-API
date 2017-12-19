7981ba6026f0cba9354a3504b1b5ce8f07fe5547
Richard Kuodis <ricard.arman.kuodis@accenture.com>
2017-12-19 14:54:13 +0200

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -9,9 +9,31 @@ const {
 } = require('./../models/todo');
 
 
+const todos = [{
+    text: 'First test todo'
+}, {
+    text: 'Second test todo'
+}, {
+    text: 'Third test todo'
+}];
+
 beforeEach((done) => {
     Todo.remove({})
-        .then(() => done());
+        .then(() => {
+            return Todo.insertMany(todos);
+        }).then(() => done());
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(3);
+            })
+            .end(done);
+    });
 });
 
 describe('POST /todos', () => {
@@ -32,7 +54,9 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({
+                    text
+                }).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -57,7 +81,7 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(3);
                     done();
                 }).catch((err) => {
                     done(err);
